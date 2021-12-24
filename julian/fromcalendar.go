ad598270a38b4bf11be8abc841ecfafd85a4d76d
Cody M. Johnson <codyj@protonmail.com>
2021-12-24 15:50:06 -0500

    update julian.FromCalendar()

--- julian/fromcalendar.go
+++ julian/fromcalendar.go
@@ -5,13 +5,21 @@ import (
 )
 
 // FromCalendar converts a calendar datetime into a Julian day
-func FromCalendar(t time.Time) float64 {
+//
+// t - time
+//
+// jd - Julian day
+func FromCalendar(t time.Time) (jd float64) {
 	A := (1461 * (t.Year() + 4800 + (int(t.Month())-14)/12)) / 4
 	B := (367 * (int(t.Month()) - 2 - 12*((int(t.Month())-14)/12))) / 12
 	C := (3 * ((t.Year() + 4900 + (int(t.Month())-14)/12) / 100)) / 4
 	D := t.Day() - 32075
-	H := float64(t.Hour()-12) / 24
-	M := float64(t.Minute()) / 1440
-	S := float64(t.Second()) / 86400
-	return float64(A+B-C+D) + H + M + S
+	H := float64(t.Hour()-12) / HoursPerDay
+	M := float64(t.Minute()) / MinutesPerDay
+	S := float64(t.Second()) / SecondsPerDay
+	_, offset := t.Zone()
+	Z := float64(offset) / SecondsPerDay
+
+	jd = float64(A+B-C+D) + H + M + S + Z
+	return
 }
--- julian/fromcalendar_test.go
+++ julian/fromcalendar_test.go
@@ -9,10 +9,10 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// TestFromCalendar
+// TestFromCalendar tests FromCalendar()
 func TestFromCalendar(t *testing.T) {
 	// stub
-	timeStr := "2000-01-01T18:11:10Z"
+	timeStr := "2000-01-01T18:11:10-01:00"
 	tm, err := time.Parse(time.RFC3339, timeStr)
 	require.NoError(t, err)
 
@@ -20,5 +20,5 @@ func TestFromCalendar(t *testing.T) {
 	jd := FromCalendar(tm)
 
 	// assertion
-	assert.Equal(t, 2.4515452577546295e+06, jd)
+	assert.Equal(t, 2.451545216087963e+06, jd)
 }
