46e9561606657260c2854ab94305cb0ee61a7224
Cody M. Johnson <codyj@protonmail.com>
2021-12-24 15:50:23 -0500

    constants

---
+++ julian/constants.go
@@ -0,0 +1,9 @@
+package julian
+
+const (
+	SecondsPerDay = 86400.0
+	MinutesPerDay = 1440.0
+	HoursPerDay   = 24.0
+
+	J2000 = 2451545.0
+)
---
+++ solar/constants.go
@@ -0,0 +1,6 @@
+package solar
+
+const (
+	M0 = 357.5291
+	M1 = 0.98560028
+)
