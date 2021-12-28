6a05aced8dbbac1f29c5b7dd17f0b026404fdee0
Cody M. Johnson <codyj@protonmail.com>
2021-12-28 14:20:55 -0500

    constants

--- solar/geomeanlongitude.go
+++ solar/geomeanlongitude.go
@@ -1,13 +1,11 @@
 package solar
 
-import "github.com/codymj/solarcalc/julian"
-
 // GeoMeanLongitude calculates the geometric mean longitude of the sun.
 //
 // jd - Julian day
 //
 // L0 - decimal degrees
 func GeoMeanLongitude(jd float64) (L0 float64) {
-	L0 = 280.46646 + 0.9856474*(jd-julian.J2000)
+	L0 = 280.46646 + 0.9856474*(jd-J2000)
 	return
 }
