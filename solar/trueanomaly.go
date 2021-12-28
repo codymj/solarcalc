b8c1f88c528b319c82add603a6f8a1f70739b19c
Cody M. Johnson <codyj@protonmail.com>
2021-12-28 14:21:10 -0500

    true anomaly

---
+++ solar/trueanomaly.go
@@ -0,0 +1,33 @@
+package solar
+
+import (
+	"math"
+)
+
+const (
+	DegreeToRadian = math.Pi / 180
+
+	C1Earth = 1.9148
+	C2Earth = 0.0200
+	C3Earth = 0.0003
+	C4Earth = 0.0000
+	C5Earth = 0.0000
+	C6Earth = 0.0000
+)
+
+// TrueAnomaly is the sum of the mean anomaly (M) and equation of center (C).
+//
+// C - equation of center result
+//
+// v - true anomaly (v = C + M)
+func TrueAnomaly(jd float64) (v float64) {
+	M := MeanAnomaly(jd)
+	MRad := M * DegreeToRadian
+
+	C := C1Earth*math.Sin(MRad) + C2Earth*math.Sin(2*MRad) +
+		C3Earth*math.Sin(3*MRad) + C4Earth*math.Sin(4*MRad) +
+		C5Earth*math.Sin(5*MRad) + C6Earth*math.Sin(6*MRad)
+
+	v = M + C
+	return
+}
---
+++ solar/trueanomaly_test.go
@@ -0,0 +1,19 @@
+package solar
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// TestTrueAnomaly tests MeanAnomaly()
+func TestTrueAnomaly(t *testing.T) {
+	// stub
+	jd := 2453097.0
+
+	// invocation
+	v := TrueAnomaly(jd)
+
+	// assertion
+	assert.Equal(t, 89.09488529793867, v)
+}
